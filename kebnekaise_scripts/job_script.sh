8e3a0302db9d24e330cc9a2a85aaf1b73a2b4d26
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-09-26 13:52:05 +0200

    Typos

--- kebnekaise_scripts/job_script.sh
+++ kebnekaise_scripts/job_script.sh
@@ -7,9 +7,9 @@
 module purge
 module load GCC/12.3.0 OpenMPI/4.1.5 Stable-Baselines3/2.3.2 gym-pybullet-drones/2.0.0-3d7b12edd4915a27e6cec9f2c0eb4b5479f7735e
 
-cp -r environments/HoverCrazyflieSim2Real.py $TMPDIR
-cp -r python_scripts/learning_script.py $TMPDIR
-cp -r python_scripts/execute_secuencial_learning.py $TPMDIR
+cp -r ../environments/HoverCrazyflieSim2Real.py $TMPDIR
+cp -r ../python_scripts/learning_script.py $TMPDIR
+cp -r ../python_scripts/execute_secuencial_learning.py $TPMDIR
 
 cd $TMPDIR
 
