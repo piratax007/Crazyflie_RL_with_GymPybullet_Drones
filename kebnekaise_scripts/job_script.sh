aff145722417276923c0c226139ecfa9f38ffa98
Fausto Mauricio Lagos Suarez <fausto@b-an01.hpc2n.umu.se>
2024-10-01 15:09:25 +0200

    Changed the gym-pybullet version to cuda version

--- kebnekaise_scripts/job_script.sh
+++ kebnekaise_scripts/job_script.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH -A hpc2n2024-120
 #SBATCH -t 00:20:00
-#SBATCH -n 1
-#SBATCH --job-name=hello_gympybullets_in_kebnekaise
+#SBATCH -n 10
+#SBATCH --job-name=Crazyflie_training
 
 module purge
-module load GCC/12.3.0 OpenMPI/4.1.5 Stable-Baselines3/2.3.2 gym-pybullet-drones/2.0.0-3d7b12edd4915a27e6cec9f2c0eb4b5479f7735e
+module load GCC/12.3.0 OpenMPI/4.1.5 Stable-Baselines3/2.3.2 gym-pybullet-drones/2.0.0-3d7b12e-CUDA-12.1.1
 module load GCC/12.3.0 tensorboard/2.15.1
 
 cd ..
