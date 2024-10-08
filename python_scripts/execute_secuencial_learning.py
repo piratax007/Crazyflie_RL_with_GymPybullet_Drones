83d569b9afa39c9ed2c6d1944fb2f4099fa06a59
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-10-08 10:17:39 +0200

    Training without curriculum learning, standard hyperparameters, sed 42

--- python_scripts/execute_secuencial_learning.py
+++ python_scripts/execute_secuencial_learning.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from environments.HoverCrazyflieSim2Real import HoverCrazyflieSim2Real
+from environments.WithoutCurriculumLearning import WithoutCurriculumLearning
 from python_scripts.learning_script import run_learning
 
 print("""############# Base training #############
@@ -8,13 +8,13 @@ Target = [0 0 1 Nan Nan Nan]
 ##################################################
 """)
 
-results = run_learning(environment=HoverCrazyflieSim2Real,
-                       learning_id="Sim2Real_CL_SUB-TASK_1_rew-800_env-4_sed-42_no-cuda",
+results = run_learning(environment=WithoutCurriculumLearning,
+                       learning_id="Training_Without-Curriculum-Learning_standard-hyperparameters_sed-42",
                        continuous_learning=False,
                        parallel_environments=4,
                        time_steps=int(100e6),
                        stop_on_max_episodes=dict(stop=False, episodes=0),
-                       stop_on_reward_threshold=dict(stop=True, threshold=800.),
+                       stop_on_reward_threshold=dict(stop=False, threshold=800.),
                        save_checkpoints=dict(save=True, save_frequency=250000)
                        )
 
