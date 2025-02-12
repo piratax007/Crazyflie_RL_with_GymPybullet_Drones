3e6c66e6f4430100ea6f40bfa42842607ca058d9
Fausto M. Lagos S <piratax007@protonmail.ch>
2025-02-12 12:03:29 +0100

    Added execution arguments to control these from the job script

--- Alvis_scripts/job_script.sh
+++ Alvis_scripts/job_script.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-#SBATCH -A hpc2n2024-120
+#SBATCH -A NAISS2024-5-442 -p alvis
 #SBATCH -t 12:00:00
 #SBATCH -n 1
 #SBATCH -c 1
+#SBATCH --gpus-per-node=A40:1
 
-ml purge > /dev/null 2>&1 # No need to see the output of this
+ml purge > /dev/null 2>&1
 ml GCC/12.3.0 OpenMPI/4.1.5
-ml gym-pybullet-drones/2.0.0-3d7b12edd4915a27e6cec9f2c0eb4b5479f7735e
-ml TensorFlow/2.15.1-CUDA-12.1.1
+ml gym-pybullet-drones/2.0.0-3d7b12e
+ml tensorboard/2.15.1-gfbf-2023a
 
 cd ..
 
-python3 -m python_scripts.execute_secuencial_learning.py
+python3 -m python_scripts.execute_secuencial_learning.py --environment EjcCLStage1 --learning-id 'EJC_CL_Stage1' --seed 90
--- python_scripts/execute_secuencial_learning.py
+++ python_scripts/execute_secuencial_learning.py
@@ -1,25 +1,71 @@
 #!/usr/bin/env python3
-from environments.ObS12Stage1 import ObS12Stage1
+import argparse
+
+from environments.ejc_cl_stage1 import EjcCLStage1
 from python_scripts.learning_script import run_learning
 
-print("""############# Base training #############
-Starting = [0 0 0 0 0 0]
-Target = [0 0 1 Nan Nan Nan]
-##################################################
-""")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Single Agent learning')
+    parser.add_argument(
+        '--environment',
+        default=EjcCLStage1,
+        type=str,
+        help='An imported environment'
+    )
+    parser.add_argument(
+        '--learning-id',
+        type=str,
+        help='A string to be added to the results directory name'
+    )
+    parser.add_argument(
+        '--continuous-learning',
+        default=False,
+        type=bool,
+        help='A boolean indicating whether to use continuous learning or not'
+    )
+    parser.add_argument(
+        '--time-steps',
+        default=int(10e7),
+        type=int,
+        help='The number of time steps'
+    )
+    parser.add_argument(
+        '--seed',
+        default=90,
+        type=int,
+        help='Seed for reproducibility'
+    )
+    parser.add_argument(
+        '--stop-on-reward-threshold-flag',
+        default=True,
+        type=bool,
+        help='A boolean indicating whether to stop on reward threshold'
+    )
+    parser.add_argument(
+        '--stop-on-reward-threshold-threshold',
+        default=6000.,
+        type=float,
+        help='A float indicating whether to stop on reward threshold'
+    )
 
-results = run_learning(environment=ObS12Stage1,
-                       learning_id="ACTION-SPACE-VEL",
-                       continuous_learning=False,
-                       parallel_environments=4,
-                       time_steps=int(10e7),
-                       stop_on_max_episodes=dict(stop=False, episodes=0),
-                       stop_on_reward_threshold=dict(stop=True, threshold=610.),
-                       save_checkpoints=dict(save=True, save_frequency=250000)
-                       )
+    args = parser.parse_args()
 
-print(f"""
-################# Learning End ########################
-Results: {results}
-#######################################################
-""")
+    reward_stop_flag = args.stop_on_reward_threshold_flag
+    reward_stop_threshold = args.stop_on_reward_threshold_threshold
+
+    results = run_learning(environment=args.environment,
+                           learning_id=args.learning_id,
+                           continuous_learning=args.continuous_learning,
+                           parallel_environments=4,
+                           time_steps=int(args.time_steps),
+                           seed=args.seed,
+                           stop_on_max_episodes=dict(stop=False, episodes=0),
+                           stop_on_reward_threshold=dict(stop=reward_stop_flag, threshold=reward_stop_threshold),
+                           save_checkpoints=dict(save=True, save_frequency=1000000)
+                           )
+
+    print(f"""
+    ################# Learning End ########################
+    Results: {results}
+    #######################################################
+    """)
