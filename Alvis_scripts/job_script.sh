85b87eaa5609bb531f4b02139c3a6e8c2a094eed
Fausto <piratax007@protonmail.ch>
2025-02-12 14:26:05 +0100

    Launched first stage CL training

--- Alvis_scripts/job_script.sh
+++ Alvis_scripts/job_script.sh
@@ -2,14 +2,15 @@
 #SBATCH -A NAISS2024-5-442 -p alvis
 #SBATCH -t 12:00:00
 #SBATCH -n 1
-#SBATCH -c 1
-#SBATCH --gpus-per-node=A40:1
+#SBATCH -c 2
+#SBATCH --gpus-per-node=A100:1
 
 ml purge > /dev/null 2>&1
 ml GCC/12.3.0 OpenMPI/4.1.5
 ml gym-pybullet-drones/2.0.0-3d7b12e
-ml tensorboard/2.15.1-gfbf-2023a
+ml TensorFlow/2.15.1-foss-2023a-CUDA-12.1.1
 
 cd ..
-
+echo "Training started at $(date)"
 python3 -m python_scripts.execute_secuencial_learning --learning-id 'EJC_CL_Stage1_seed-7' --seed 7
+echo "Training finished at $(date)"
