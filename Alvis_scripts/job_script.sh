5a3591c7ecef368e2711109282c9b25d15db80b9
Fausto <piratax007@protonmail.ch>
2025-02-13 18:06:19 +0100

    Added training at 100 Hz control frequency

--- Alvis_scripts/job_script.sh
+++ Alvis_scripts/job_script.sh
@@ -12,7 +12,8 @@ ml TensorFlow/2.15.1-foss-2023a-CUDA-12.1.1
 cd ..
 echo "Training started at $(date)"
 
-python3 -m python_scripts.execute_sequential_learning  --environment 'EjcCLStage2' --learning-id 'EJC_Agent-B_90_CPU' \
---algorithm 'ppo' --parallel-environments 1 --seed 90 --time-steps 100000000 --stop-on-reward-threshold-flag Fase
+python3 -m python_scripts.execute_sequential_learning  --environment 'EjcCLStage1CtrlFreq100' --learning-id \
+'EJC_stage-1_ctrl-freq-100_39' \
+--algorithm 'ppo' --parallel-environments 4 --seed 39 --time-steps 30000000 --stop-on-reward-threshold-flag Fase
 
 echo "Training finished at $(date)"
