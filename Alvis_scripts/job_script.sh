af357f6987a3184926a1184c943dc46e3694ec9a
Fausto <piratax007@protonmail.ch>
2025-02-13 14:55:24 +0100

    Modify to launch Alvis jobs without GPU

--- Alvis_scripts/job_script.sh
+++ Alvis_scripts/job_script.sh
@@ -2,8 +2,7 @@
 #SBATCH -A NAISS2024-5-442 -p alvis
 #SBATCH -t 48:00:00
 #SBATCH -n 1
-#SBATCH -c 2
-#SBATCH --gpus-per-node=T4:1
+#SBATCH -C NOGPU
 
 ml purge > /dev/null 2>&1
 ml GCC/12.3.0 OpenMPI/4.1.5
@@ -13,7 +12,7 @@ ml TensorFlow/2.15.1-foss-2023a-CUDA-12.1.1
 cd ..
 echo "Training started at $(date)"
 
-python3 -m python_scripts.execute_secuencial_learning  --environment 'EjcCLStage3' --learning-id 'EJC_WCL_DDPG_39' \
---algorithm 'ddpg' --parallel-environments 1 --seed 39 --time-steps 100000000 --stop-on-reward-threshold-flag Fase
+python3 -m python_scripts.execute_secuencial_learning  --environment 'EjcCLStage3' --learning-id 'EJC_WCL_PPO_39_CPU' \
+--algorithm 'ppo' --parallel-environments 1 --seed 39 --time-steps 100000000 --stop-on-reward-threshold-flag Fase
 
 echo "Training finished at $(date)"
