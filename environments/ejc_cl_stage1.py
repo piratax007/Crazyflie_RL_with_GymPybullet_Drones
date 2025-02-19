c9a59719e315dad05e51f673bb209465c28135a8
Fausto M. Lagos S <piratax007@protonmail.ch>
2025-02-19 16:40:05 +0100

    Increased the control frequency to 240

--- environments/ejc_cl_stage1.py
+++ environments/ejc_cl_stage1.py
@@ -14,7 +14,7 @@ class EjcCLStage1(BaseRLAviary):
             target_xyzs = np.array([0, 0, 1]),
             physics: Physics = Physics.PYB_GND,
             pybullet_frequency: int = 240,
-            ctrl_freq: int = 30,
+            ctrl_freq: int = 240,
             gui = False,
             record = False,
             observation_space: ObservationType = ObservationType.KIN,
