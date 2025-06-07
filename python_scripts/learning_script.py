e6e351261d8e5515803ced594fcc7422e2be1efe
fausto <piratax007@protonmail.ch>
2025-06-07 12:43:13 +0200

    Changed default seed to None

--- python_scripts/learning_script.py
+++ python_scripts/learning_script.py
@@ -179,7 +179,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '--seed',
-        default=0,
+        default=None,
         type=int,
         help='Random seed for reproducibility'
     )
