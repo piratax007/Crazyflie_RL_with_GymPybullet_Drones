5ba24d316a93ef1ae507d6cd25b19587a068b4be
fausto <piratax007@protonmail.ch>
2025-07-08 02:35:06 +0200

    Training using 33 grams as Crazyflie mass with updated hyperparameters

--- kebnekaise_scripts/job_script.sh
+++ kebnekaise_scripts/job_script.sh
@@ -13,7 +13,7 @@ cd ..
 echo "Training started at $(date)"
 
 python3 -m python_scripts.execute_sequential_learning  --environment 'CLStage2Sim2Real' \
- --learning-id 'Stage1_S2R_e2e_Crazyflie-mass-33_200-100-frequency' \
+ --learning-id 'Stage1_S2R_e2e_Crazyflie-mass-33_200-100-frequency_updated-hyperparameters' \
 --algorithm 'ppo' --parallel-environments 4 --time-steps 30000000
 
 echo "Training finished at $(date)"
