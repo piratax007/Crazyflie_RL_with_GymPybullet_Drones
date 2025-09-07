b9116295f846612101bdc42356b17714a82ba2b9
fausto <piratax007@protonmail.ch>
2025-09-07 17:18:06 +0200

    Second stage of training with big dr

--- kebnekaise_scripts/job_script.sh
+++ kebnekaise_scripts/job_script.sh
@@ -14,8 +14,9 @@ echo "Training started at $(date)"
 
 echo "CURRENT PATH $(pwd)"
 
-python3 -m python_scripts.execute_sequential_learning  --environment 'CLStage1Sim2RealDomainRandomization' \
- --learning-id 'First-Stage_s2r-e2e-dr' --algorithm 'ppo' --parallel-environments 4 \
+python3 -m python_scripts.execute_sequential_learning  --environment 'CLStage2Sim2RealDomainRandomization' \
+ --learning-id 'Second-Stage_s2r-e2e-dr' --algorithm 'ppo' --parallel-environments 4 \
+ --continuous-learning True --path-to-previous-model 'results/save-First-Stage_s2r-e2e-dr-09.05.2025_11.23.36' \
  --time-steps 30000000
 
 echo "Training finished at $(date)"
