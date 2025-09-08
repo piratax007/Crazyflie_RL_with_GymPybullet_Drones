4a72d32a25fb5d1daee5e947a84c17bb024c1885
fausto <piratax007@protonmail.ch>
2025-09-08 10:19:05 +0200

    Training for third stage of training using high domain randomization and quaternion instead of Euler angles (a7255c4)

--- kebnekaise_scripts/job_script.sh
+++ kebnekaise_scripts/job_script.sh
@@ -14,9 +14,9 @@ echo "Training started at $(date)"
 
 echo "CURRENT PATH $(pwd)"
 
-python3 -m python_scripts.execute_sequential_learning  --environment 'CLStage3Sim2RealDomainRandomization' \
- --learning-id 'Third-Stage_s2r-e2e-dr' --algorithm 'ppo' --parallel-environments 4 \
- --continuous-learning True --path-to-previous-model 'results/save-Second-Stage_s2r-e2e-dr-09.07.2025_17.15.35' \
+python3 -m python_scripts.execute_sequential_learning  --environment 'CLStage3S2RE2EDRQuat' \
+ --learning-id 'Third-Stage_s2r-e2e-dr-quaternion' --algorithm 'ppo' --parallel-environments 4 \
+ --continuous-learning True --path-to-previous-model 'results/save-Second-Stage_s2r-e2e-dr-quaterion-09.07.2025_17.29.20' \
  --time-steps 30000000
 
 echo "Training finished at $(date)"
